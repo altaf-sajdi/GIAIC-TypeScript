6268dc74e6df878d96e888f0c291dcdf94ec32ef
altaf-sajdi <sajdiedu@gmail.com>
2024-07-19 03:23:43 -0700

    write a tuples code

---
+++ 01_tuples/tuple.ts
@@ -0,0 +1,15 @@
+/* 1=Typed Arrays
+      A tuple is a typed array with a pre-defined length and types for each index.
+
+      Tuples are great because they allow each element in the array to be a known type of value.
+
+      To define a tuple, specify the type of each element in the array:*/
+
+      let myTupele:[number, string,string,boolean];
+
+      myTupele=[12,"sajdi","{name = altaf, age =25}", true];
+      console.log("myTuple orgenal", myTupele);
+
+      myTupele.push("same thing others tuples");
+
+      console.log("other tuples by using push comands=",myTupele)
